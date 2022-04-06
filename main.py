54c6c2a96f9959e0cd01ef1beb7e0f580dbe0d2a
Raffaele Cataldo <kemanicataldo@gmail.com>
2022-04-06 15:08:38 +0200

    added .env variables

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ from twilio.twiml.voice_response import Dial, VoiceResponse, Say
 from dotenv import load_dotenv    
 load_dotenv()  
 
-PHONE_NUMBER = os.getenv('PHONE_NUMBER')
+TARGET_PHONE_NUMBER = os.getenv('TARGET_PHONE_NUMBER')
 
 response = VoiceResponse()
-response.dial(PHONE_NUMBER)
+response.dial(TARGET_PHONE_NUMBER)
 response.say('Goodbye')
 
 print(response)
