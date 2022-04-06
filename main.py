0b14ba9f96549f80e1d4bb68af101869e25cfc1f
Raffaele Cataldo <kemanicataldo@gmail.com>
2022-04-06 15:24:47 +0200

    reverted to flask, basic code complete

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 import os
-from twilio.twiml.voice_response import Dial, VoiceResponse, Say
+from flask import Flask
+from twilio.twiml.voice_response import Dial, VoiceResponse
 from dotenv import load_dotenv    
 load_dotenv()  
 
 TARGET_PHONE_NUMBER = os.getenv('TARGET_PHONE_NUMBER')
 
-response = VoiceResponse()
-response.dial(TARGET_PHONE_NUMBER)
-response.say('Goodbye')
+app = Flask(__name__)
 
-print(response)
+@app.route('/', methods=["GET", "POST"])
+def call():
+    response = VoiceResponse()
+    response.dial(TARGET_PHONE_NUMBER)
+
+    return str(response)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
